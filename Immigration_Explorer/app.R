8fb176f6bc450fd14aeea2ae29aee689afe018f3
mengxit <mtan@mde.harvard.edu>
2019-11-22 16:58:48 -0500

    Shiny - With one homepage pic

--- Immigration_Explorer/app.R
+++ Immigration_Explorer/app.R
@@ -18,44 +18,121 @@ english_country <- read_csv("english_country.csv")
                             
 immigration_continent <- read_csv("immigration_continent.csv")
                                 
-immigration_country <- read_csv("immigration_country.csv")
-                                
+immigration_continent_long <- read_csv("immigration_continent_long.csv")
 
+immigration_country <- read_csv("immigration_country.csv")
+
+immigration_country_long <- read_csv("immigration_country_long.csv")
+
+
+# percent function for formatting
+percent <- function(x, digits = 1, format = "f", ...) {
+    paste0(formatC(100 * x, format = format, digits = digits, ...), "%")
+}
+
+                            
 # define UI for application that draws a histogram
 # create Navigation bar for both overview and by country
 # create an input for selecting specific country to examine
 
 ui<- navbarPage("Gov1005 Final Project: US Immigration Explorer",
                 tabPanel("Overview",
-                         mainPanel(
-                             "PLACE HOLDER"
-                         )),
-                tabPanel("A Closer Look",
-                         headerPanel("PLACE HOLDER"),
-                         sidebarPanel(
-                            selectInput("variable", "Country:",
-                                        list("China" = "China, People's Republic", 
-                                            "Mexico" = "Mexico", 
-                                            "India" = "India"))
+                         headerPanel("IMMIGRATION INTO US BY SOURCE COUNTRY - WILL UPDATE"),
+                         imageOutput("overview", width = "20%", height = "20%"),
+                         imageOutput("barchart")
+
+                        
                          ),
-                         imageOutput("trendchart"))
-)
+                tabPanel("A Closer Look",
+                         headerPanel("CLOSER LOOK INTO A SOURCE COUNTRY - WILL UPDATE"),
+                         fluidRow(
+                          column(8, align="left",
+                          selectInput("variable", "Country:",
+                                                list("China" = "China, People's Republic", 
+                                                     "Mexico" = "Mexico", 
+                                                     "India" = "India")),
+                         plotOutput("trendchart")),
+                         column(3, align = "right",
+                         plotOutput("donut")))
+))
 
 
 # define server
 server <- function(input, output, session){
   
+  # render the opening page
+  output$overview <- renderImage(
+    list(src = "intro_graph.png"),
+    deleteFile = FALSE
+  )
+  
+  # render the bar chart on the opening page
+  
+  
 # render the trend chart on the left
   output$trendchart <- renderPlot({
     
     # this chart is going to be the graph shown on the left
-    immigration_country_current <- immigration_country %>% 
+    immigration_country_current <- immigration_country_long %>% 
       filter(country == input$variable)
   
-    graph_left <- ggplot(immigration_country_current) + geom_col(aes(x = year, y = total)) + labs(title = paste0("Current Country :", input$variable))
+    graph_left <- ggplot(immigration_country_current, 
+                         aes(x = year, y = count, fill = admission_class)) +
+        geom_area(position = 'stack') + 
+        labs(y = "Total Immigrants", x = "Year", 
+             fill = "Admission Class", 
+             title = paste0("Immigration Overview from ", input$variable,  " to US")) + 
+        scale_fill_discrete(labels = c("Diversity", "Employment",
+                                       "Immediate Relatives", "Other Relatives", "Refugee", "Other")) +  
+        scale_x_continuous(breaks = c(2009, 2011, 2013, 2015, 2017))
     
     print(graph_left)
   })
+ 
+# render language donut chart
+  output$donut <- renderPlot({
+      
+      # this chart is going to be shown on the right
+      english_country_current <- english_country %>%
+          filter(country == input$variable)
+      
+      # make a summary chart
+      english_country_current_summary <- english_country_current %>%
+          summarize(total = sum(total_number), 
+                    total_very_well = sum(population_very_well),
+                    not_very_well = total - total_very_well) %>%
+          select(total_very_well, not_very_well)
+      
+      # gather information from long to short format
+      english_country_current_summary <- english_country_current_summary%>%
+          gather(key = "english", value = "population",total_very_well:not_very_well)
+      
+      # compute percentages
+      english_country_current_summary$fraction = english_country_current_summary$population / sum(english_country_current_summary$population)
+      
+      # compute the cumulative percentages (top of each rectangle)
+      english_country_current_summary$ymax = cumsum(english_country_current_summary$fraction)
+      
+      # compute the bottom of each rectangle
+      english_country_current_summary$ymin = c(0, head(english_country_current_summary$ymax, n=-1))
+      
+      donut <- ggplot(english_country_current_summary, 
+          aes(ymax = ymax, ymin=ymin, 
+          xmax=4, xmin=3, fill=english)) +
+          geom_rect() +
+          coord_polar(theta = "y") +
+          xlim(c(2, 4)) + 
+          scale_fill_discrete(labels = c("Not Very Well", "Very Well")) +
+          labs(title = paste0(percent(english_country_current_summary$fraction[1]) ,
+                              "  immigrants from ", input$variable, " Speak English Very Well"),
+                             fill = "English Speaking Ability", 
+                        caption = "Average during 2007 - 2016") +
+          theme(axis.text.y=element_blank(), 
+                axis.ticks=element_blank())
+      
+      print(donut)
+  })  
+  
 }
 
 # Run the application 
